0a32e10dc0ea694a5c8caec4ceba8992d3af7ed4
Clifford Harvey <charvey@mychirotouch.com>
2020-11-25 10:29:12 -0800

    register

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import React, { Component } from 'react'
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
-
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: 'f33286c4252c4be2831316717977a9c1'
+});
+
 
 const particlesOptions = {
   particles: {
@@ -19,18 +29,88 @@ const particlesOptions = {
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className='particles'
-        params={particlesOptions}
-      />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignedIn: false,
+    }
+  }
+
+  calculateLocation = data => {
+    const clarifaiFace = data.outputs.[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage')
+    const width =  Number(image.width);
+    const height = Number(image.height);
+
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height),
+    }
+  }
+
+  displayFaceBox = box => {
+    this.setState({ box });
+  }
+
+  onInputChange = (e) => {
+    this.setState({ input: e.target.value });
+  }
+
+  onButtonSubmit = (e) => {
+    this.setState({ imageUrl: this.state.input });
+
+    app.models.initModel({id: Clarifai.FACE_DETECT_MODEL, version: "aa7f35c01e0642fda5cf400f543e7c40"})
+      .then(faceDerectlModel => faceDerectlModel.predict(this.state.input))
+      .then(response => this.displayFaceBox(this.calculateLocation(response)))
+      .catch(err => console.log(err));
+  }
+
+  onRouteChange = route => {
+    if (route === 'signout') {
+      this.setState({ isSignedIn: false});
+    } else if ( route === 'home') {
+      this.setState({ isSignedIn: true });
+    }
+    this.setState({ route });
+  }
+
+  render() {
+    const { imageUrl, box, route, isSignedIn } = this.state;
+    return (
+      <div className="App">
+        <Particles className='particles'
+          params={particlesOptions}
+        />
+        <Navigation onRouteChange={this.onRouteChange} isSignedIn={isSignedIn}/>
+        { route === 'home'
+          ? <div>
+              <Logo />
+              <Rank />
+              <ImageLinkForm
+                onInputChange={this.onInputChange}
+                onButtonSubmit={this.onButtonSubmit}
+              />
+              <FaceRecognition
+                imageUrl={imageUrl}
+                box={box}
+              />
+            </div>
+          : (
+            route === 'signin'
+              ? <Signin onRouteChange={this.onRouteChange} />
+              : <Register onRouteChange={this.onRouteChange} />
+            )
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './FaceRecognition.css'
+
+const FaceRecognition = ({ imageUrl, box }) => {
+  const style = {
+    top: box.topRow,
+    right: box.rightCol,
+    bottom: box.bottomRow,
+    left: box.leftCol,
+  }
+
+  return (
+    <div className='center ma'>
+      <div className='absolute mt2'>
+        <img id='inputimage' alt='' src={imageUrl} width='500px' height='auto'/>
+        <div className='bounding-box' style={style}></div>
+      </div>
+    </div>
+  );
+}
+
+export default FaceRecognition;
